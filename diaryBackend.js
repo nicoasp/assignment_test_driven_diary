406f612f5a5959972784eae7724af2195db7d277
nicoasp <nicolas.amaya@gmail.com>
2017-04-07 23:20:57 +0200

    finish entriesWithTag

--- diaryBackend.js
+++ diaryBackend.js
@@ -18,7 +18,27 @@ function Diary(){
 		this._entries.push(newMessage);
 	}
 	this.entries = function() {
-	    return this._entries;
+	  return this._entries;
+	}
+	this.tags = function() {
+		let _tags = [];
+		this._entries.forEach((entry) => {
+			entry.tags.forEach((tag) => {
+				if (!_tags.includes(tag)) {
+					_tags.push(tag);
+				}
+			})
+		})
+		return _tags.sort();
+	}
+	this.entriesWithTag = function(tag) {
+		let _entriesWithTag = [];
+		this._entries.forEach((entry) => {
+			if (entry.tags.includes(tag)) {
+				_entriesWithTag.push(entry);
+			}
+		})
+		return _entriesWithTag;
 	}
 }
 
--- spec/diaryBackendSpec.js
+++ spec/diaryBackendSpec.js
@@ -106,34 +106,96 @@ describe('Diary', function(){
 	describe("entries function", function() {
 	    
 	    describe("if entries is empty", function() {
-	        it("returns an empty array", function() {
-	            expect(diary.entries()).toEqual([]);
-	        });
+        it("returns an empty array", function() {
+            expect(diary.entries()).toEqual([]);
+        });
 	        
 	    });
 	    describe("if entries are present", function() {
 	        
-
-    	    beforeEach(function(){
+    	  beforeEach(function(){
     			diary.entry("I'm standing outside Brad's house #yolo");
     			diary.entry("OMG. What have I done? #sorrynotsorry");
     			diary.entry("What if dogs were not dogs? #dogs I done? #sorrynotsorry");
+					diary.entry("What if dogs were not dogs? ###dogs");
+					diary.entry(" # some stuff i guess");
+    		});
+    		
+    		it("returns the correct number of entries", function() {
+    		  expect(diary.entries().length).toEqual(5);
+    		});
+
+    		it("entries have body, date and tags", function() {
+    			let keys = Object.keys(diary.entries()[0]).sort();
+    		  expect(keys).toEqual(['body', 'tags', 'time']);
+    		});
+    		    		
+	    });	    	    
+	});
+
+	describe('tags method', function() {
+
+		describe('if no entries', function(){
+			it ('returns an empty array', function(){
+				expect(diary.tags()).toEqual([]);
+			})
+		})
+
+		describe("if no tags", function(){
+  	  beforeEach(function(){
+  			diary.entry("I'm standing outside Brad's house");
+  			diary.entry("OMG. What have I done?");
+  			diary.entry("What if dogs were not dogs? I done?");
+				diary.entry("What if dogs were not dogs?");
+				diary.entry(" # some stuff i guess");
+  		});			
+
+			it('returns an empty array', function(){
+				expect(diary.tags()).toEqual([]);
+			})
+		})
+
+		describe('if tags present', function() {
+		  beforeEach(function(){
+  			diary.entry("I'm standing outside Brad's house #yolo");
+  			diary.entry("OMG. What have I done? #sorrynotsorry");
+  			diary.entry("What if dogs were not dogs? #dogs I done? #sorrynotsorry");
 				diary.entry("What if dogs were not dogs? ###dogs");
 				diary.entry(" # some stuff i guess");
-    		});
-    		
-    		it("returns the correct number of entries", function() {
-    		    expect(diary.entries().length).toEqual(5);
-    		});
-    		it("returns the correct number of entries", function() {
-    		    expect(diary.entries().length).toEqual(5);
-    		});
-    		
-    		
-	    });
-	    
-	    
-	    
+  		});
+
+  		it('returns a list of unique tags', function() {
+  			expect(diary.tags()).toEqual(['dogs', 'sorrynotsorry', 'yolo']);
+  		})
+		})
+	})
+
+	describe('entriesWithTag method', function() {
+
+	  beforeEach(function(){
+			diary.entry("I'm standing outside Brad's house #yolo");
+			diary.entry("OMG. What have I done? #sorrynotsorry");
+			diary.entry("What if dogs were not dogs? #dogs I done? #sorrynotsorry");
+			diary.entry("What if dogs were not dogs? ###dogs");
+			diary.entry(" # some stuff i guess");
+		});
+
+
+		it ("returns an empty array if tag doesn't exist", function(){
+			expect(diary.entriesWithTag('bananas')).toEqual([]);
+		})
+
+		it ("returns the correct number of entries if tag exists", function(){
+			expect(diary.entriesWithTag('dogs').length).toEqual(2);
+		})
+
+
 	})
 	
 });
+
+
+
+
+
+
