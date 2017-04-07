1e35a8c92132ca361880f9eb042cbf8f5b6c7feb
nicoasp <nicolas.amaya@gmail.com>
2017-04-07 20:10:05 +0200

    finish .entry capturing tags

--- diaryBackend.js
+++ diaryBackend.js
@@ -3,10 +3,22 @@ function Diary(){
 	this.entry = function(body, date){
 		var newMessage = {};
 		newMessage.body = body;
-		newMessage.time = Date.now();
+		newMessage.time = date || Date.now();
+		newMessage.tags = [];
+
+		let tagFinder = /#\w+/g
+		let matches = body.match(tagFinder);
+
+		if (matches) {
+			matches.forEach((match) => {
+				newMessage.tags.push(match.slice(1));
+			})
+		}
+		
 		this.entries.push(newMessage);
 	}
 }
+
 
 
 
--- spec/diaryBackendSpec.js
+++ spec/diaryBackendSpec.js
@@ -5,51 +5,73 @@ const Diary = require('../diaryBackend');
 describe('Diary', function(){
     
 	describe('entry function', function(){
-		var diary, date1;
+		var diary;
 
 		beforeEach(function(){
 			diary = new Diary();
-		})
-
-		it('contains entry message', function() {
-			diary.entry("Brad is everything to me.");
-			let bodies = diary.entries.map((entry) => {
-				return entry.body;
-			})
-			expect(bodies).toContain("Brad is everything to me.");
-		})
-
-		it('contains another entry message', function() {
-			diary.entry("<3 Braaaad <3");
-			let bodies = diary.entries.map((entry) => {
-				return entry.body;
-			})
-			expect(bodies).toContain("<3 Braaaad <3");
-		})
-
-		it ('records the date', function(){
-		    let message = "Hiiiiiiii";
-			diary.entry(message);
-
-			date1 = Date.now();
-
-            let time = diary.entries[0].time;
-			
-			expect(time).toBeCloseTo(date1);
-
 		});
-		
-		it ('accepts a date object and adds it to the entry', function() {
+
+		describe('entries with only body', function(){
+			var date;
+
+			beforeEach(function(){
+				diary.entry("Brad is everything to me.");
+				date = Date.now();
+				diary.entry("<3 Braaaad <3");
+			});			
+
+			it('stores a message', function() {				
+				let bodies = diary.entries.map((entry) => {
+					return entry.body;
+				})
+				expect(bodies).toContain("Brad is everything to me.");
+			})
+
+			it('stores a different message', function() {				
+				let bodies = diary.entries.map((entry) => {
+					return entry.body;
+				})
+				expect(bodies).toContain("<3 Braaaad <3");
+			})
+
+			it ('records the date', function(){
+	      let time = diary.entries[0].time;			
+				expect(time).toBeCloseTo(date);
+			});
+		});
+
+		describe('entries with date', function(){
+			it ('accepts a date object and adds it to the entry', function() {
 		    let date = Date.parse("Mon, 25 Dec 1995 13:30:00 GMT");
 		    diary.entry("YOYOYOYOYOYOYO", date);
-		    
-		    
-		    
+		    		    
 		    let time1 = diary.entries[0].time;
-            expect(time1).toEqual(date);
+        expect(time1).toEqual(date);
+			});
+		});
 
-		    
-		    
+		describe('entries with tags', function(){
+
+			beforeEach(function(){
+				diary.entry("I'm standing outside Brad's house #yolo");
+				diary.entry("OMG. What have I done? #sorrynotsorry");
+			});						
+
+			it ('records the right tag for a message', function(){
+				let body = "I'm standing outside Brad's house #yolo";
+				let entry = diary.entries.find((el) => {
+					return el.body === body;
+				})
+				expect(entry.tags).toEqual(['yolo']);
+			})
+
+			it ('records the right tag for a different message', function(){
+				let body = "OMG. What have I done? #sorrynotsorry";
+				let entry = diary.entries.find((el) => {
+					return el.body === body;
+				})
+				expect(entry.tags).toEqual(['sorrynotsorry']);
+			})			
 		})
 		
 		
