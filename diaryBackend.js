f574610c029e23c514a8e3ee53a97eb67c824082
nicoasp <nicolas.amaya@gmail.com>
2017-04-07 19:05:01 +0200

    set up describe entry method

--- diaryBackend.js
+++ diaryBackend.js
@@ -1,9 +1,24 @@
+function Diary(){
+	this.entries = [];
+	this.entry = function(body){
+		var newMessage = {};
+		newMessage.body = body;
+		this.entries.push(newMessage);
+	}
+}
+
+
+
+module.exports = Diary;
+
+
+
 /***
  * File data structure
  * [
         {
             "time": Date(),
-            "note": "<string>",
+            "body": "<string>",
             "tags": []
         },...
     ]
--- spec/diaryBackendSpec.js
+++ spec/diaryBackendSpec.js
@@ -1,1 +1,39 @@
 const Diary = require('../diaryBackend');
+
+
+
+describe('Diary', function(){
+
+	describe('entry function', function(){
+		var diary, date1;
+
+		beforeEach(function(){
+			diary = new Diary();
+			diary.entry("Brad is everything to me.");
+			date1 = Date.now();
+			diary.entry("<3 Braaaad <3");
+		})
+
+		it('contains entry message', function() {		
+			let bodies = diary.entries.map((entry) => {
+				return entry.body;
+			})
+			expect(bodies).toContain("Brad is everything to me.");
+		})
+
+		it('contains another entry message', function() {
+			let bodies = diary.entries.map((entry) => {
+				return entry.body;
+			})
+			expect(bodies).toContain("<3 Braaaad <3");
+		})
+
+		it ('records the date', function(){
+
+		})
+	})
+
+
+
+
+})
